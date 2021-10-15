646f752544bd06f4a1538daa5ee1eae7a6b2718b
João Felipe P <jf.pelliccione@gmail.com>
2021-10-15 13:43:48 -0300

    Botando app no ar - parte 2!

---
+++ build/precache-manifest.4da7fb06d82145638337ae7e5daacdcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "014a2a783846fd7f82adf2d708c7956b",
+    "url": "/trybe_frontend_2-movie_cards/index.html"
+  },
+  {
+    "revision": "a52f771293b3c8134a29",
+    "url": "/trybe_frontend_2-movie_cards/static/css/main.a046a3d6.chunk.css"
+  },
+  {
+    "revision": "0d4f78d6a282aeb41de1",
+    "url": "/trybe_frontend_2-movie_cards/static/js/2.9a6a2542.chunk.js"
+  },
+  {
+    "revision": "a52f771293b3c8134a29",
+    "url": "/trybe_frontend_2-movie_cards/static/js/main.fcc20d6c.chunk.js"
+  },
+  {
+    "revision": "1c7391cd67356308ebea",
+    "url": "/trybe_frontend_2-movie_cards/static/js/runtime-main.fae02bea.js"
+  },
+  {
+    "revision": "1a4de1ea9de49ac144537e5fe722231c",
+    "url": "/trybe_frontend_2-movie_cards/static/media/background.1a4de1ea.jpg"
+  }
+]);
